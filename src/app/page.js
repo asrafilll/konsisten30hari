fba6a723e85b8b70838b3ad40f8d052bf65593ce
asrafilll <asrafilr@gmail.com>
2024-01-27 09:28:08 +0700

    feat: svent summary

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import { EventSummary } from "@/components/EventSummary";
 import { HeroSection } from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <div className="h-[220px]"></div>
+      <EventSummary />
     </>
   );
 }
---
+++ src/components/EventSummary.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const EventSummary = () => {
+  return (
+    <div>
+      <div className="w-[50%] flex flex-col gap-4 ">
+        <div className="text-xl text-gray-300 leading-relaxed">
+          Bulan Ramadhan tak lama lagi datang. <br />
+          Alangkah malang diri kita jika bulan yang penuh berkah ini berlalu
+          begitu saja tanpa curahan ampunan dan pahala dari-Nya.
+        </div>
+        <div className="text-xl text-gray-300 leading-relaxed">
+          Semoga Allah mempertemukan kita dengan bulan yang mulia ini,
+          melarutkan kita dalam kelezatan beribadah dan bermunajat kepada-Nya,
+          menangisi dosa dan kesalahan kita. <br />
+          <br />
+          Ya Allah Ya Rabbi, pertemukanlah kami dengannya Maka persiapkanlah
+          diri ini dengan sabaik-baik persiapan menyambutnya agar
+          #ramadhanlebihbaik
+        </div>
+        <div className="h-[220px]"></div>
+      </div>
+      <div className="grid grid-cols-3 justify-items-center w-full">
+        <div className="w-20 h-100 bg-slate-700">a</div>
+        <div>b</div>
+        <div>c</div>
+      </div>
+    </div>
+  );
+};
