0abe34802ae31f4eb48a32b6f608cd4572ccea47
asrafilll <asrafilr@gmail.com>
2024-01-28 09:31:20 +0700

    add footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import { EventSummary } from "@/components/EventSummary";
+import { Footer } from "@/components/Footer";
 import { HeroSection } from "@/components/HeroSection";
 import { RegistrationForm } from "@/components/RegistrationForm";
 import { RundownEvent } from "@/components/RundownEvent";
@@ -13,6 +14,8 @@ export default function Home() {
       <RundownEvent />
       <div className="h-[220px]"></div>
       <RegistrationForm />
+      <div className="h-[220px]"></div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Image from "next/image";
+
+export const Footer = () => {
+  return (
+    <div className="w-full flex flex-col mx-auto text-center">
+      <div className="text-3xl text-white">
+        Jejaksahabat x Konsisten30Hari x Brightside Street Market
+      </div>
+      <div className="pt-44 text-xl">Sponsored By</div>
+      <div className="mx-auto">
+        <Image
+          src="/img/sponsors.png"
+          width={500}
+          height={500}
+          alt="Sponsor Event"
+        />
+      </div>
+    </div>
+  );
+};
